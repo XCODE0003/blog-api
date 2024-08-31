8de6669d23de6ce30a389df9c09b9c2a7018c155
KASRO1 <nikita150489@mail.ru>
2024-08-31 20:12:06 +0300

    Код доделан до конца

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -17,6 +19,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
     protected $fillable = [
         'name',
         'email',
